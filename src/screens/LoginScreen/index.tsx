45bfaa62313c3933f3ee84dbbb467125b8b60dcf
StevenRose4816 <steven_jangoh@yahoo.com>
2024-01-05 12:13:33 -0500

    feat(login): setup basic login components

--- src/screens/LoginScreen/index.tsx
+++ src/screens/LoginScreen/index.tsx
@@ -1,12 +1,68 @@
-import { FC } from "react";
-import { View, Text } from "react-native";
+import {FC} from 'react';
+import {View, Text, TextInput, TouchableOpacity} from 'react-native';
 
-import styles from "./styles";
+import styles from './styles';
+import {useNavigation} from '@react-navigation/native';
 
 const LoginScreen: FC = () => {
+  const {navigate} = useNavigation();
   return (
-    <View>
-      <Text>{"LoginScreen"}</Text>
+    <View style={{flex: 3}}>
+      <View style={{flex: 1, backgroundColor: 'white'}} />
+      <View style={{flex: 2, backgroundColor: 'white'}}>
+        <Text style={{marginLeft: 10}}>{'email'}</Text>
+        <TextInput
+          style={{
+            backgroundColor: 'white',
+            marginHorizontal: 10,
+            borderRadius: 5,
+            minHeight: 50,
+            borderWidth: 1,
+            borderColor: 'black',
+          }}
+        />
+        <Text style={{marginLeft: 10, marginTop: 10}}>{'password'}</Text>
+        <TextInput
+          style={{
+            backgroundColor: 'white',
+            marginHorizontal: 10,
+            borderRadius: 5,
+            minHeight: 50,
+            borderWidth: 1,
+            borderColor: 'black',
+          }}
+        />
+        <TouchableOpacity
+          onPress={() => navigate('SignupScreen')}
+          style={{
+            marginTop: 10,
+            alignItems: 'flex-end',
+            marginRight: 20,
+          }}>
+          <Text>{'Sign Up'}</Text>
+        </TouchableOpacity>
+        <View style={{flex: 1, justifyContent: 'flex-end', marginBottom: 60}}>
+          <TouchableOpacity
+            style={{
+              backgroundColor: 'blue',
+              minHeight: 50,
+              justifyContent: 'center',
+              borderRadius: 5,
+              marginHorizontal: 10,
+              marginTop: 20,
+            }}>
+            <Text
+              style={{
+                color: 'white',
+                fontSize: 21,
+                fontWeight: '600',
+                textAlign: 'center',
+              }}>
+              {'Login'}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
