73cbf590d389c0d4d62017e90f24ca68b6f6c69a
StevenRose4816 <steven_jangoh@yahoo.com>
2024-01-07 15:01:12 -0500

    added error modal to Sign Up screen

--- src/screens/SignupScreen/index.tsx
+++ src/screens/SignupScreen/index.tsx
@@ -1,5 +1,5 @@
 import {FC, useState} from 'react';
-import {View, Text, TouchableOpacity, TextInput} from 'react-native';
+import {View, Text, TouchableOpacity, TextInput, Modal} from 'react-native';
 import auth from '@react-native-firebase/auth';
 
 import styles from './styles';
@@ -7,12 +7,21 @@ import styles from './styles';
 const SignupScreen: FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
-  const login = async () => {
+  const toggleModal = () => {
+    setModalVisible(!modalVisible);
+  };
+
+  const signUp = async () => {
     try {
       await auth().createUserWithEmailAndPassword(email, password);
     } catch (e: any) {
       console.log(e);
+      setErrorMessage(e);
+      toggleModal();
     }
   };
   return (
@@ -47,9 +56,23 @@ const SignupScreen: FC = () => {
           onChangeText={setPassword}
           secureTextEntry={true}
         />
+        <Text style={{marginLeft: 10, marginTop: 10}}>{'phone number'}</Text>
+        <TextInput
+          style={{
+            backgroundColor: 'white',
+            marginHorizontal: 10,
+            borderRadius: 5,
+            minHeight: 50,
+            borderWidth: 1,
+            borderColor: 'black',
+          }}
+          value={phoneNumber}
+          onChangeText={setPhoneNumber}
+          secureTextEntry={true}
+        />
         <View style={{flex: 1, justifyContent: 'flex-end', marginBottom: 60}}>
           <TouchableOpacity
-            onPress={login}
+            onPress={signUp}
             style={{
               backgroundColor: 'blue',
               minHeight: 50,
@@ -68,6 +91,55 @@ const SignupScreen: FC = () => {
               {'Sign Up'}
             </Text>
           </TouchableOpacity>
+          <Modal
+            visible={modalVisible}
+            animationType={'slide'}
+            transparent={true}
+            onRequestClose={toggleModal}>
+            <View
+              style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: 'rgba(0, 0, 0, 0.5)',
+              }}>
+              <View
+                style={{
+                  backgroundColor: 'darkorange',
+                  minHeight: 300,
+                  width: '80%',
+                  justifyContent: 'center',
+                  borderRadius: 5,
+                  padding: 20,
+                }}>
+                <Text style={{textAlign: 'center', color: 'white'}}>
+                  {errorMessage + '\n'}
+                </Text>
+                <TouchableOpacity
+                  onPress={toggleModal}
+                  style={{
+                    marginTop: 20,
+                    backgroundColor: 'blue',
+                    minHeight: 50,
+                    justifyContent: 'center',
+                    borderRadius: 5,
+                    marginHorizontal: 10,
+                  }}>
+                  <Text
+                    style={{
+                      textAlign: 'center',
+                      color: 'white',
+                      fontSize: 21,
+                      fontWeight: '600',
+                      backgroundColor: 'blue',
+                      borderRadius: 10,
+                    }}>
+                    {'Close'}
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </Modal>
         </View>
       </View>
     </View>
