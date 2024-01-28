2f47e76c933c135c4c1edd02ee9b51ed36f5811a
StevenRose4816 <steven_jangoh@yahoo.com>
2024-01-28 18:51:40 -0500

    conditionally render caught data

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -5,7 +5,6 @@ import {onValue, ref, set} from 'firebase/database';
 import styles from './styles';
 import {useAppSelector} from '../../hooks';
 import {db} from '../HomeScreen/FirebaseConfigurations';
-import {snapshotEqual} from 'firebase/firestore';
 
 const HomeScreen: FC = () => {
   const logout = () => {
@@ -14,6 +13,7 @@ const HomeScreen: FC = () => {
 
   const user = auth().currentUser;
   const userId = auth().currentUser?.uid;
+  const [caughtData, setCaughtData] = useState(undefined);
 
   function create(userId: string | undefined) {
     set(ref(db, 'users/' + userId), {
@@ -23,7 +23,22 @@ const HomeScreen: FC = () => {
       phoneNumber: phoneNumber,
     })
       .then(() => {
-        console.log('data pushed to db');
+        console.log('db updated yo');
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  function update(userId: string | undefined) {
+    set(ref(db, 'users/' + userId), {
+      username: fullName,
+      email: email,
+      address: address,
+      phoneNumber: phoneNumber,
+    })
+      .then(() => {
+        console.log('data updated db');
       })
       .catch(error => {
         console.log(error);
@@ -34,7 +49,9 @@ const HomeScreen: FC = () => {
     const countRef = ref(db, 'users/' + userId);
     onValue(countRef, snapshot => {
       const data = snapshot.val();
+      setCaughtData(data);
       console.log('Here is the returned data: ', data);
+      console.log('caught data=', caughtData);
     });
   }
 
@@ -71,79 +88,85 @@ const HomeScreen: FC = () => {
           }}>
           {'Hello ' + email + '!\n'}
         </Text>
-        <Text style={{fontSize: 16, marginLeft: 10, fontWeight: '600'}}>
-          {"\nLet's start by getting some informaton."}
-        </Text>
-        <Text style={{marginLeft: 10}}>{'\n\nfull name'}</Text>
-        <TextInput
-          value={fullName}
-          placeholder=" full name"
-          onChangeText={val => setFullName(val)}
-          secureTextEntry={false}
-          style={{
-            backgroundColor: 'white',
-            marginHorizontal: 10,
-            marginBottom: 10,
-            marginTop: 10,
-            borderRadius: 5,
-            minHeight: 50,
-            borderWidth: 1,
-            borderColor: 'black',
-          }}></TextInput>
-        <Text style={{marginLeft: 10}}>{'\nphone number'}</Text>
-        <TextInput
-          value={phoneNumber}
-          placeholder=" phone number"
-          onChangeText={phoneNumber => setPhoneNumber(phoneNumber)}
-          secureTextEntry={false}
-          style={{
-            backgroundColor: 'white',
-            marginHorizontal: 10,
-            marginBottom: 10,
-            marginTop: 10,
-            borderRadius: 5,
-            minHeight: 50,
-            borderWidth: 1,
-            borderColor: 'black',
-          }}></TextInput>
-        <Text style={{marginLeft: 10}}>{'\naddress'}</Text>
-        <TextInput
-          value={address}
-          placeholder=" address"
-          onChangeText={address => setAddress(address)}
-          secureTextEntry={false}
-          style={{
-            backgroundColor: 'white',
-            marginHorizontal: 10,
-            marginBottom: 10,
-            marginTop: 10,
-            borderRadius: 5,
-            minHeight: 50,
-            borderWidth: 1,
-            borderColor: 'black',
-          }}></TextInput>
+        {!caughtData && (
+          <>
+            <Text style={{fontSize: 16, marginLeft: 10, fontWeight: '600'}}>
+              {"\nLet's start by getting some informaton."}
+            </Text>
+            <Text style={{marginLeft: 10}}>{'\n\nfull name'}</Text>
+            <TextInput
+              value={fullName}
+              placeholder=" full name"
+              onChangeText={val => setFullName(val)}
+              secureTextEntry={false}
+              style={{
+                backgroundColor: 'white',
+                marginHorizontal: 10,
+                marginBottom: 10,
+                marginTop: 10,
+                borderRadius: 5,
+                minHeight: 50,
+                borderWidth: 1,
+                borderColor: 'black',
+              }}></TextInput>
+            <Text style={{marginLeft: 10}}>{'\nphone number'}</Text>
+            <TextInput
+              value={phoneNumber}
+              placeholder=" phone number"
+              onChangeText={phoneNumber => setPhoneNumber(phoneNumber)}
+              secureTextEntry={false}
+              style={{
+                backgroundColor: 'white',
+                marginHorizontal: 10,
+                marginBottom: 10,
+                marginTop: 10,
+                borderRadius: 5,
+                minHeight: 50,
+                borderWidth: 1,
+                borderColor: 'black',
+              }}></TextInput>
+            <Text style={{marginLeft: 10}}>{'\naddress'}</Text>
+            <TextInput
+              value={address}
+              placeholder=" address"
+              onChangeText={address => setAddress(address)}
+              secureTextEntry={false}
+              style={{
+                backgroundColor: 'white',
+                marginHorizontal: 10,
+                marginBottom: 10,
+                marginTop: 10,
+                borderRadius: 5,
+                minHeight: 50,
+                borderWidth: 1,
+                borderColor: 'black',
+              }}></TextInput>
+          </>
+        )}
       </View>
-      <TouchableOpacity
-        onPress={onPress}
-        style={{
-          backgroundColor: 'blue',
-          minHeight: 50,
-          justifyContent: 'center',
-          borderRadius: 5,
-          marginHorizontal: 10,
-          marginTop: 10,
-          marginBottom: 0,
-        }}>
-        <Text
+      {!caughtData && (
+        <TouchableOpacity
+          onPress={onPress}
           style={{
-            color: 'white',
-            fontSize: 21,
-            fontWeight: '600',
-            textAlign: 'center',
+            backgroundColor: 'blue',
+            minHeight: 50,
+            justifyContent: 'center',
+            borderRadius: 5,
+            marginHorizontal: 10,
+            marginTop: 10,
+            marginBottom: 0,
           }}>
-          {'Submit'}
-        </Text>
-      </TouchableOpacity>
+          <Text
+            style={{
+              color: 'white',
+              fontSize: 21,
+              fontWeight: '600',
+              textAlign: 'center',
+            }}>
+            {'Submit'}
+          </Text>
+        </TouchableOpacity>
+      )}
       <TouchableOpacity
         onPress={logout}
         style={{
