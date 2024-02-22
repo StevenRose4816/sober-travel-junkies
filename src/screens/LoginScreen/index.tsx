6bef8b3839886037246c88ec443a212022380aa6
StevenRose4816 <steven_jangoh@yahoo.com>
2024-02-22 10:14:02 -0500

    added custom font

--- src/screens/LoginScreen/index.tsx
+++ src/screens/LoginScreen/index.tsx
@@ -187,7 +187,7 @@ const LoginScreen: FC = () => {
             marginLeft: 10,
             marginRight: 10,
             marginTop: 5,
-            fontFamily: 'Vonique64',
+            fontFamily: 'HighTide-Sans',
           }}>
           {'CREATE ACCOUNT'}
         </Text>
