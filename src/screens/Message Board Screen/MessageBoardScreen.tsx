6c5fdaa32782055b45129e739d3f9c07ef607568
StevenRose4816 <steven_jangoh@yahoo.com>
2024-02-28 13:03:00 -0500

    saving progress on message board

--- src/screens/Message Board Screen/MessageBoardScreen.tsx
+++ src/screens/Message Board Screen/MessageBoardScreen.tsx
@@ -4,6 +4,7 @@ import {
   Dimensions,
   FlatList,
   Image,
+  Modal,
   StyleSheet,
   Text,
   TextInput,
@@ -24,15 +25,17 @@ const MessageBoardScreen: FC = () => {
   const [messages, setMessages] = useState<Message[]>([]);
   const [newMessage, setNewMessage] = useState<string>('');
   const [newTitle, setNewTitle] = useState<string>('');
+  const [titlefromDB, setTitlefromDB] = useState<string>('');
   const [postedTitle, setPostedTitle] = useState<string>('');
+  const [modalVisible, setModalVisible] = useState(false);
 
   useEffect(() => {
     readData();
     console.log('Messages in db: ', messages);
   }, []);
 
-  const create = async (messages: any) => {
-    await set(ref(db, 'messages/'), {messages});
+  const create = async (messages: any, title: any) => {
+    await set(ref(db, 'messages/'), {messages, title});
     console.log('db created/updated');
   };
 
@@ -43,6 +46,7 @@ const MessageBoardScreen: FC = () => {
       if (snapshot.exists()) {
         const data = snapshot.val();
         console.log('Data: ', data);
+        setTitlefromDB(data.title);
         setMessages(data.messages);
         console.log('First Message: ', data.messages[0].text);
       } else {
@@ -53,15 +57,17 @@ const MessageBoardScreen: FC = () => {
     }
   };
 
-  const addMessage = async () => {
+  const onSend = async () => {
     if (newMessage.trim() !== '') {
       const updatedMessages = [
         ...messages,
         {text: newMessage, id: Math.random().toString()},
       ];
       setMessages(updatedMessages);
-      await create(updatedMessages);
+      await create(updatedMessages, newTitle);
       setNewMessage('');
+      onSetTitle(newTitle);
+      toggleModal();
     }
   };
 
@@ -74,11 +80,24 @@ const MessageBoardScreen: FC = () => {
     }
   };
 
+  const toggleModal = () => {
+    setModalVisible(!modalVisible);
+  };
+
   return (
     <View style={styles.container}>
+      <View style={{}}>
+        <Text
+          style={{
+            textAlign: 'center',
+            marginBottom: 10,
+          }}>
+          {'Messages'}
+        </Text>
+      </View>
       {postedTitle && (
         <View>
-          <Text>{'Test'}</Text>
+          <Text>{postedTitle}</Text>
         </View>
       )}
       <FlatList
@@ -94,19 +113,54 @@ const MessageBoardScreen: FC = () => {
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.input}
+          placeholder="Set the title of your message..."
+          value={newTitle}
+          onChangeText={title => setNewTitle(title)}
+        />
+        <TextInput
+          style={styles.input}
           placeholder="Type your message..."
           value={newMessage}
           onChangeText={newMessage => setNewMessage(newMessage)}
         />
-        <Button title="Send" onPress={addMessage} />
-        <TextInput
-          style={styles.input}
-          placeholder="Type your message..."
-          value={newTitle}
-          onChangeText={title => setNewTitle(title)}
-        />
-        <Button title="Set Title" onPress={() => onSetTitle(newTitle)} />
+        <TouchableOpacity
+          style={{
+            backgroundColor: '#b6e7cc',
+            borderRadius: 5,
+            width: 100,
+            borderWidth: 1,
+            borderColor: '#eee7da',
+          }}
+          onPress={onSend}>
+          <Text
+            style={{
+              color: '#0c0b09',
+              fontSize: 12,
+              fontWeight: '600',
+              marginBottom: 10,
+              marginTop: 10,
+              marginRight: 10,
+              marginLeft: 10,
+              textAlign: 'center',
+              fontFamily: 'HighTide-Sans',
+            }}>
+            {'Send'}
+          </Text>
+        </TouchableOpacity>
       </View>
+      <Modal
+        visible={modalVisible}
+        animationType={'slide'}
+        transparent={true}
+        onRequestClose={toggleModal}>
+        <View style={styles.modalView5}>
+          <TouchableOpacity onPress={toggleModal}>
+            <View style={styles.modalView6}>
+              <Text>{'Modal'}</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -126,8 +180,10 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
   inputContainer: {
-    height: 200,
+    backgroundColor: '#eee7da',
+    height: 150,
     justifyContent: 'flex-start',
+    borderRadius: 5,
   },
   input: {
     flex: 1,
@@ -136,6 +192,18 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#ccc',
     borderRadius: 8,
+  },
+  modalView5: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalView6: {
+    backgroundColor: '#b6e7cc',
+    height: 300,
+    width: 300,
+    borderRadius: 5,
   },
 });
 
