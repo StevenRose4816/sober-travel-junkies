b9b8e69637a0655fc7a9cb38f5e3e7e722e8bb4e
StevenRose4816 <steven_jangoh@yahoo.com>
2024-04-04 16:05:17 -0400

    added generateDisableDates function to diable dates on calender.

--- src/screens/BooneScreen/index.tsx
+++ src/screens/BooneScreen/index.tsx
@@ -1,13 +1,40 @@
 import React, {FC, useState} from 'react';
-import {Text, View} from 'react-native';
-import CalendarPicker from 'react-native-calendar-picker';
+import {Text, TouchableOpacity, View} from 'react-native';
+import CalendarPicker, {
+  DateChangedCallback,
+} from 'react-native-calendar-picker';
 
 const BooneScreen: FC = () => {
-  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const [selectedStartDate, setSelectedStartDate] = useState<Date | null>(null);
+  const [selectedEndDate, setSelectedEndDate] = useState<Date | null>(null);
+  const minDate = new Date();
+  const maxDate = new Date(2025, 1, 1);
 
-  const handleDateChange = (date: Date) => {
-    setSelectedDate(date);
-    console.log('date: ', selectedDate);
+  const generateDisabledDates = () => {
+    const startDate = new Date(2025, 9, 1); // Month is 0-indexed
+    const endDate = new Date(2025, 9, 31);
+    const datesArray = [];
+    for (let d = startDate; d <= endDate; d.setDate(d.getDate() + 1)) {
+      datesArray.push(new Date(d));
+    }
+    return datesArray;
+  };
+
+  const handleDateChange: DateChangedCallback = (date, type) => {
+    if (type === 'END_DATE') {
+      setSelectedEndDate(date);
+    } else {
+      setSelectedStartDate(date);
+      setSelectedEndDate(null);
+    }
+  };
+
+  const startDate = selectedStartDate ? selectedStartDate.toString() : '';
+  const endDate = selectedEndDate ? selectedEndDate.toString() : '';
+  const [showCalender, setShowCalender] = useState(false);
+
+  const onPressViewCalender = () => {
+    setShowCalender(showCalender => !showCalender);
   };
 
   return (
@@ -23,14 +50,36 @@ const BooneScreen: FC = () => {
         }}>
         {'Valle Crucis 24'}
       </Text>
-      <CalendarPicker
-        onDateChange={handleDateChange}
-        selectedDayStyle={{backgroundColor: 'red'}}
-      />
+      <TouchableOpacity
+        style={{
+          backgroundColor: '#b6e7cc',
+          borderRadius: 5,
+          width: 120,
+        }}
+        onPress={() => onPressViewCalender()}>
+        <Text style={{fontFamily: 'HighTide-Sans', textAlign: 'center'}}>
+          View Calender
+        </Text>
+      </TouchableOpacity>
+      {showCalender && (
+        <View style={{marginTop: 50}}>
+          <CalendarPicker
+            onDateChange={handleDateChange}
+            selectedDayStyle={{backgroundColor: 'red'}}
+            todayBackgroundColor={'grey'}
+            textStyle={{fontFamily: 'HighTide-Sans'}}
+            allowRangeSelection={true}
+            allowBackwardRangeSelect={true}
+            minDate={minDate}
+            maxDate={maxDate}
+            disabledDates={generateDisabledDates()}
+          />
+        </View>
+      )}
       <Text
         style={{fontFamily: 'HighTide-Sans', marginLeft: 20, marginTop: 20}}>
-        SELECTED DATES:{' '}
-        {selectedDate ? selectedDate.toString() : 'None Selected'}
+        SELECTED START DATE: {startDate || 'None'} {'\n'}
+        SELECTED END DATE: {endDate || 'None'}
       </Text>
     </View>
   );
