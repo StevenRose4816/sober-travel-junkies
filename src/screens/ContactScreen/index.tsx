4b022f8a967782d9cd72baa93f2e96e7b49d3953
StevenRose4816 <steven_jangoh@yahoo.com>
2024-04-15 12:12:56 -0400

    saving changes --ContactScreen

--- src/screens/ContactScreen/index.tsx
+++ src/screens/ContactScreen/index.tsx
@@ -1,6 +1,6 @@
 import {RouteProp, useRoute} from '@react-navigation/native';
 import {FC} from 'react';
-import {ScrollView, Text, View} from 'react-native';
+import {ScrollView, Text} from 'react-native';
 import Contacts from 'react-native-contacts';
 import {AppStackParams} from '../../navigation/types';
 import Routes from '../../navigation/routes';
@@ -8,20 +8,53 @@ import Routes from '../../navigation/routes';
 const ContactScreen: FC = () => {
   const route = useRoute<RouteProp<AppStackParams, Routes.contactScreen>>();
   const contacts = route.params;
-  const contactsPrinted = JSON.stringify(contacts);
+
   const filteredContacts = contacts.filter(
-    i => i.familyName !== '' && i.givenName !== '',
+    contact => contact.familyName !== '' && contact.givenName !== '',
   );
-  const names = contacts.map(
-    i => i.familyName + ', ' + i.givenName + ' ' + i.phoneNumbers[0]['number'],
-  );
-  console.log('filteredContacts: ', JSON.stringify(filteredContacts));
-  console.log('names: ', names);
+
+  const mappedContacts = filteredContacts.map(contact => {
+    const mobilePhoneNumber =
+      contact.phoneNumbers.find(phoneNumber => phoneNumber.label === 'mobile')
+        ?.number || undefined;
+
+    const mainPhoneNumber =
+      contact.phoneNumbers.find(phoneNumber => phoneNumber.label === 'main')
+        ?.number || undefined;
+
+    const homeFaxPhoneNumber =
+      contact.phoneNumbers.find(phoneNumber => phoneNumber.label === 'home fax')
+        ?.number || undefined;
+
+    const workPhoneNumber =
+      contact.phoneNumbers.find(phoneNumber => phoneNumber.label === 'work')
+        ?.number || undefined;
+
+    const homePhoneNumber =
+      contact.phoneNumbers.find(phoneNumber => phoneNumber.label === 'home')
+        ?.number || undefined;
+
+    return {
+      familyName: contact.familyName,
+      givenName: contact.givenName,
+      phoneNumbers: {
+        mobile: mobilePhoneNumber,
+        main: mainPhoneNumber,
+        homeFax: homeFaxPhoneNumber,
+        work: workPhoneNumber,
+        home: homePhoneNumber,
+      },
+    };
+  });
+
+  console.log('Contacts: ', JSON.stringify(contacts));
+  console.log('Mapped Contacts: ', JSON.stringify(mappedContacts));
 
   return (
     <ScrollView>
-      <Text>{contactsPrinted}</Text>
+      <Text>{JSON.stringify(mappedContacts)}</Text>
     </ScrollView>
   );
 };
+
 export default ContactScreen;
