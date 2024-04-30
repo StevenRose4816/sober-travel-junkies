20395d1c214af93d6e0e0c55155f8cbb035b90a0
StevenRose4816 <steven_jangoh@yahoo.com>
2024-04-30 12:17:37 -0400

    saving progress --VisionBoardScreen

--- src/screens/VisionBoardScreen/index.tsx
+++ src/screens/VisionBoardScreen/index.tsx
@@ -7,12 +7,14 @@ import {
   ImageBackground,
   Modal,
   Text,
+  TextInput,
   TouchableOpacity,
   View,
 } from 'react-native';
 import Routes from '../../navigation/routes';
 import {AppStackParams, NavPropAny} from '../../navigation/types';
 import Draggable from 'react-native-draggable';
+import {vi} from 'date-fns/locale/vi';
 
 interface IProps {
   navigation?: NativeStackNavigationProp<any, any>;
@@ -28,6 +30,9 @@ export const VisionBoardScreen: FC = () => {
   const screenHeight = Dimensions.get('window').height;
   const screenWidth = Dimensions.get('window').width;
   const [modalVisible, setModalVisible] = useState(false);
+  const [addNote, setAddNote] = useState(true);
+  const [newNote, setNewNote] = useState('');
+  const [visibleNote, setVisibleNote] = useState('');
 
   const toggleModal = () => {
     setModalVisible(!modalVisible);
@@ -44,6 +49,16 @@ export const VisionBoardScreen: FC = () => {
     console.log('backgroundPhoto: ', backgroundPhoto);
   }, [selectedImage, backgroundPhoto]);
 
+  const onAddNote = () => {
+    // setAddNote(!addNote);
+    toggleModal();
+  };
+
+  const onSubmitNote = () => {
+    setVisibleNote(newNote);
+    toggleModal();
+  };
+
   return (
     <View
       style={{
@@ -51,6 +66,7 @@ export const VisionBoardScreen: FC = () => {
         justifyContent: 'flex-start',
         padding: 8,
         borderRadius: 5,
+        position: 'relative',
       }}>
       <ImageBackground
         style={{flex: 1}}
@@ -73,18 +89,63 @@ export const VisionBoardScreen: FC = () => {
             minX={0}
             minY={40}
             maxX={375}
-            maxY={680}
-            renderSize={56}
+            maxY={640}
             renderColor="#fb445c"
             renderText="A"
             isCircle
             onShortPressRelease={() => console.log('touched!!')}>
             <Image
               style={{height: 50, width: 50}}
-              source={{uri: selectedImage}}
+              source={
+                selectedImage
+                  ? {
+                      uri: selectedImage,
+                    }
+                  : require('../../Images/camerapictureicon.png')
+              }
               resizeMode="stretch"></Image>
           </Draggable>
         )}
+        {addNote && (
+          <Draggable
+            x={200}
+            y={100}
+            minX={0}
+            minY={40}
+            maxX={375}
+            maxY={640}
+            onShortPressRelease={() => console.log('touched!!')}>
+            <ImageBackground
+              style={{
+                height: 60,
+                width: 100,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+              source={require('../../Images/sticky.png')}
+              resizeMode="stretch">
+              <Text
+                style={{
+                  fontFamily: 'HighTide-Sans',
+                  fontSize: 8,
+                  maxWidth: 40,
+                }}>
+                {visibleNote}
+              </Text>
+            </ImageBackground>
+          </Draggable>
+        )}
+      </ImageBackground>
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          position: 'absolute',
+          bottom: 0,
+          left: 15,
+          right: 15,
+        }}>
         <TouchableOpacity
           onPress={onPressOpenImagePicker}
           style={{
@@ -94,21 +155,62 @@ export const VisionBoardScreen: FC = () => {
             borderWidth: 1,
             borderColor: '#eee7da',
             height: 50,
-            alignSelf: 'flex-end',
-            marginTop: screenHeight * 0.75,
-            marginRight: 10,
+            justifyContent: 'center',
+            alignItems: 'center',
           }}>
           <Text
             style={{
               color: '#0c0b09',
-              marginTop: 10,
               textAlign: 'center',
               fontFamily: 'HighTide-Sans',
             }}>
-            {'Open image picker'}
+            Open image picker
           </Text>
         </TouchableOpacity>
-      </ImageBackground>
+        <TouchableOpacity
+          onPress={() => console.log('save snapshot')}
+          style={{
+            backgroundColor: '#e7b6cc',
+            borderRadius: 50,
+            width: 80,
+            height: 80,
+            margin: 20,
+            borderWidth: 1,
+            borderColor: '#eee7da',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text
+            style={{
+              color: '#0c0b09',
+              fontFamily: 'HighTide-Sans',
+              textAlign: 'center',
+            }}>
+            Save snapshot
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={onAddNote}
+          style={{
+            backgroundColor: '#e7b6cc',
+            borderRadius: 5,
+            width: 100,
+            borderWidth: 1,
+            borderColor: '#eee7da',
+            height: 50,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text
+            style={{
+              color: '#0c0b09',
+              fontFamily: 'HighTide-Sans',
+              textAlign: 'center',
+            }}>
+            Add Note
+          </Text>
+        </TouchableOpacity>
+      </View>
       <Modal
         visible={modalVisible}
         animationType={'slide'}
@@ -148,8 +250,47 @@ export const VisionBoardScreen: FC = () => {
                   marginTop: 5,
                   fontFamily: 'Vonique64',
                 }}>
-                {'Submit'}
+                Add Note
               </Text>
+              <TextInput
+                value={newNote}
+                placeholder=" Note"
+                onChangeText={note => setNewNote(note)}
+                secureTextEntry={false}
+                style={{
+                  fontFamily: 'HighTide-Sans',
+                  backgroundColor: '#eee7da',
+                  borderRadius: 5,
+                  height: 50,
+                  width: 150,
+                  borderWidth: 1,
+                  borderColor: '#5A6472',
+                  borderBottomWidth: 3,
+                  marginTop: 40,
+                  textAlign: 'center',
+                }}></TextInput>
+              <TouchableOpacity
+                onPress={onSubmitNote}
+                style={{
+                  backgroundColor: '#e7b6cc',
+                  borderRadius: 5,
+                  width: 100,
+                  borderWidth: 1,
+                  borderColor: '#eee7da',
+                  marginTop: 80,
+                }}>
+                <Text
+                  style={{
+                    color: '#0c0b09',
+                    fontSize: 12,
+                    fontWeight: '600',
+                    margin: 10,
+                    textAlign: 'center',
+                    fontFamily: 'HighTide-Sans',
+                  }}>
+                  {'Submit'}
+                </Text>
+              </TouchableOpacity>
             </View>
           </View>
         </View>
